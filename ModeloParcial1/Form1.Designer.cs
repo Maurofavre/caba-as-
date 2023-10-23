eaf25d48b077f9b4c2f6b280bb255886cd4871ae
mauro <mauro@MAUROF>
2023-10-22 21:32:14 -0300

    Agregar archivos de proyecto.

---
+++ ModeloParcial1/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace ModeloParcial1
+{
+    partial class frmService
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnReporte = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.GroupBox2 = new System.Windows.Forms.GroupBox();
+            this.optCredito = new System.Windows.Forms.RadioButton();
+            this.optContado = new System.Windows.Forms.RadioButton();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkEGarantia = new System.Windows.Forms.CheckBox();
+            this.chkASoftware = new System.Windows.Forms.CheckBox();
+            this.cmbPlazo = new System.Windows.Forms.ComboBox();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.cmbTipoDispositivo = new System.Windows.Forms.ComboBox();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.GroupBox2.SuspendLayout();
+            this.GroupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(320, 167);
+            this.btnCerrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(90, 40);
+            this.btnCerrar.TabIndex = 29;
+            this.btnCerrar.Text = "C&errar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            // 
+            // btnReporte
+            // 
+            this.btnReporte.Location = new System.Drawing.Point(320, 98);
+            this.btnReporte.Margin = new System.Windows.Forms.Padding(2);
+            this.btnReporte.Name = "btnReporte";
+            this.btnReporte.Size = new System.Drawing.Size(90, 40);
+            this.btnReporte.TabIndex = 28;
+            this.btnReporte.Text = "&Reporte";
+            this.btnReporte.UseVisualStyleBackColor = true;
+            this.btnReporte.Click += new System.EventHandler(this.btnReporte_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(320, 29);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(90, 40);
+            this.btnCalcular.TabIndex = 27;
+            this.btnCalcular.Text = "&Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(316, 248);
+            this.txtTotal.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(94, 20);
+            this.txtTotal.TabIndex = 26;
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Location = new System.Drawing.Point(313, 230);
+            this.Label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(69, 13);
+            this.Label5.TabIndex = 25;
+            this.Label5.Text = "Importe Total";
+            // 
+            // GroupBox2
+            // 
+            this.GroupBox2.Controls.Add(this.optCredito);
+            this.GroupBox2.Controls.Add(this.optContado);
+            this.GroupBox2.Location = new System.Drawing.Point(27, 222);
+            this.GroupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.GroupBox2.Name = "GroupBox2";
+            this.GroupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.GroupBox2.Size = new System.Drawing.Size(248, 67);
+            this.GroupBox2.TabIndex = 24;
+            this.GroupBox2.TabStop = false;
+            this.GroupBox2.Text = "Forma de Pago";
+            // 
+            // optCredito
+            // 
+            this.optCredito.AutoSize = true;
+            this.optCredito.Location = new System.Drawing.Point(144, 27);
+            this.optCredito.Margin = new System.Windows.Forms.Padding(2);
+            this.optCredito.Name = "optCredito";
+            this.optCredito.Size = new System.Drawing.Size(58, 17);
+            this.optCredito.TabIndex = 1;
+            this.optCredito.Text = "Crédito";
+            this.optCredito.UseVisualStyleBackColor = true;
+            // 
+            // optContado
+            // 
+            this.optContado.AutoSize = true;
+            this.optContado.Checked = true;
+            this.optContado.Location = new System.Drawing.Point(47, 27);
+            this.optContado.Margin = new System.Windows.Forms.Padding(2);
+            this.optContado.Name = "optContado";
+            this.optContado.Size = new System.Drawing.Size(65, 17);
+            this.optContado.TabIndex = 0;
+            this.optContado.TabStop = true;
+            this.optContado.Text = "&Contado";
+            this.optContado.UseVisualStyleBackColor = true;
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.chkEGarantia);
+            this.GroupBox1.Controls.Add(this.chkASoftware);
+            this.GroupBox1.Location = new System.Drawing.Point(28, 138);
+            this.GroupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.GroupBox1.Size = new System.Drawing.Size(248, 69);
+            this.GroupBox1.TabIndex = 23;
+            this.GroupBox1.TabStop = false;
+            this.GroupBox1.Text = "Opciones";
+            // 
+            // chkEGarantia
+            // 
+            this.chkEGarantia.AutoSize = true;
+            this.chkEGarantia.Location = new System.Drawing.Point(61, 42);
+            this.chkEGarantia.Margin = new System.Windows.Forms.Padding(2);
+            this.chkEGarantia.Name = "chkEGarantia";
+            this.chkEGarantia.Size = new System.Drawing.Size(113, 17);
+            this.chkEGarantia.TabIndex = 3;
+            this.chkEGarantia.Text = "Extender Garantía";
+            this.chkEGarantia.UseVisualStyleBackColor = true;
+            // 
+            // chkASoftware
+            // 
+            this.chkASoftware.AutoSize = true;
+            this.chkASoftware.Location = new System.Drawing.Point(61, 17);
+            this.chkASoftware.Margin = new System.Windows.Forms.Padding(2);
+            this.chkASoftware.Name = "chkASoftware";
+            this.chkASoftware.Size = new System.Drawing.Size(117, 17);
+            this.chkASoftware.TabIndex = 0;
+            this.chkASoftware.Text = "Actualizar Software";
+            this.chkASoftware.UseVisualStyleBackColor = true;
+            // 
+            // cmbPlazo
+            // 
+            this.cmbPlazo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPlazo.FormattingEnabled = true;
+            this.cmbPlazo.Location = new System.Drawing.Point(127, 103);
+            this.cmbPlazo.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbPlazo.Name = "cmbPlazo";
+            this.cmbPlazo.Size = new System.Drawing.Size(50, 21);
+            this.cmbPlazo.TabIndex = 22;
+            this.cmbPlazo.SelectedIndexChanged += new System.EventHandler(this.cmbPlazo_SelectedIndexChanged);
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(25, 105);
+            this.Label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(88, 13);
+            this.Label4.TabIndex = 21;
+            this.Label4.Text = "Plazo de Entrega";
+            // 
+            // cmbTipoDispositivo
+            // 
+            this.cmbTipoDispositivo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTipoDispositivo.FormattingEnabled = true;
+            this.cmbTipoDispositivo.Location = new System.Drawing.Point(127, 66);
+            this.cmbTipoDispositivo.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbTipoDispositivo.Name = "cmbTipoDispositivo";
+            this.cmbTipoDispositivo.Size = new System.Drawing.Size(148, 21);
+            this.cmbTipoDispositivo.TabIndex = 20;
+            this.cmbTipoDispositivo.SelectedIndexChanged += new System.EventHandler(this.cmbTipoDispositivo_SelectedIndexChanged);
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(25, 69);
+            this.Label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(97, 13);
+            this.Label3.TabIndex = 19;
+            this.Label3.Text = "Tipo de Dispositivo";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(127, 29);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(148, 20);
+            this.txtNombre.TabIndex = 17;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(25, 32);
+            this.Label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(39, 13);
+            this.Label1.TabIndex = 15;
+            this.Label1.Text = "Cliente";
+            // 
+            // frmService
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 310);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnReporte);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.Label5);
+            this.Controls.Add(this.GroupBox2);
+            this.Controls.Add(this.GroupBox1);
+            this.Controls.Add(this.cmbPlazo);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.cmbTipoDispositivo);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.Label1);
+            this.Name = "frmService";
+            this.Text = "Smart Rep";
+            this.Load += new System.EventHandler(this.frmService_Load);
+            this.GroupBox2.ResumeLayout(false);
+            this.GroupBox2.PerformLayout();
+            this.GroupBox1.ResumeLayout(false);
+            this.GroupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btnCerrar;
+        internal System.Windows.Forms.Button btnReporte;
+        internal System.Windows.Forms.Button btnCalcular;
+        internal System.Windows.Forms.TextBox txtTotal;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.GroupBox GroupBox2;
+        internal System.Windows.Forms.RadioButton optCredito;
+        internal System.Windows.Forms.RadioButton optContado;
+        internal System.Windows.Forms.GroupBox GroupBox1;
+        internal System.Windows.Forms.CheckBox chkASoftware;
+        internal System.Windows.Forms.ComboBox cmbPlazo;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.ComboBox cmbTipoDispositivo;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.TextBox txtNombre;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.CheckBox chkEGarantia;
+    }
+}
+
---
+++ ModeloParcial1/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModeloParcial1
+{
+    /*
+    Datos para el cálculo de la reparación
+    Tipos de dispositivo a mostrar en el ComboBox: Smartphone, Tablet y Consola
+    costo base: smartphone $5500, tablet $4850 y consola $9000
+    los dias de plazo de entrega varian en función del dispositivo seleccionado:
+    smartphone: entre 2 y 10 días (mostrar 2, 3, 4, 5, 6, 7, 8, 9 y 10)
+    tablet: entre 1 y 7 días (mostrar 1, 2, 3, 4, 5, 6 y 7)
+    consola: entre 5 y 12 días (mostrar 5, 6, 7, 8, 9, 10, 11 y 12)
+    Opcionales: Actualizar software $1500, Extender la garantía $1000
+
+    Forma de pago: Crédito: se agrega un 5% de recargo al precio total
+    si es Contado se descuenta un 10% del precio total.
+    El botón calcular se habilita solamente cuando se ingrese el nombre del cliente
+    -En el evento del botón se realizará el cálculo y se mostrará su resultado en el TextBox "txtTotal", 
+     además se almacenará en un arreglo de estructura REPARACION estos datos:
+      - Cliente      string
+      - Dispositivo  string
+      - Importe      float
+     El arreglo para almacenar las reparaciones tiene capacidad para 20 registros
+
+    El botón Reporte mostrará en un cuadro de mensaje (MessageBox) la suma de todos los importes
+    registrados en el arreglo.
+     */
+
+    public partial class frmService : Form
+    {
+        const float smartph = 5500;
+        const float tablet = 4850;
+        const float consola = 9000;
+        const float Tcredito = 5;
+        const float contado = 10;
+        const float actualizar = 1500;
+        const float garantia = 100;
+
+        public struct REPORTE
+        {
+            public string nombre;
+            public string aparato;
+            public int monto;
+        }
+
+        const int Maximo = 10;
+
+        REPORTE[] reporte;
+
+        int Posicion = 0; 
+
+
+
+        public frmService()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            float resultado = 0;
+            float opcionales = 0; 
+            float recargo = 0;
+            float descuento = 0;
+            int equipo = 0;
+
+
+            // Le paso el precio de cada dispositivo a la var resultado 
+            if (cmbTipoDispositivo.SelectedIndex == 0)
+            {
+                resultado = smartph;
+            }
+            else
+            {
+                if (cmbTipoDispositivo.SelectedIndex==1)
+                {
+                    resultado = tablet;
+                }
+                else
+                {
+                    if (cmbTipoDispositivo.SelectedIndex ==2) 
+                    {
+                        resultado = consola;
+                    }
+                }
+            }
+
+            // me fijo si estan en true los chek y le paso el precio a la variables 
+            if(chkASoftware.Checked == true)
+            {
+                opcionales = opcionales + actualizar;
+            }
+            else
+            {
+                opcionales = 0;
+            }
+
+            if (chkEGarantia.Checked == true) 
+            {
+                opcionales = opcionales + garantia;
+            }
+            else
+            {
+                opcionales = 0;
+            }
+
+            // calculo el resultado mas agregados  
+
+            resultado = resultado + opcionales;
+
+            // calculo descuentos o subas 
+
+           if(optContado.Checked == true)
+            {
+                descuento = resultado * 10 / 100;
+                resultado = resultado - descuento; 
+            } else
+            {
+                recargo = resultado * 20 / 100;
+                resultado = resultado + recargo; 
+            }
+
+            MessageBox.Show("Costo de equipos: " + resultado.ToString());
+        }
+
+        private void frmService_Load(object sender, EventArgs e)
+        {
+            //Crea el arreglo 
+
+            reporte = new REPORTE[Maximo]; 
+
+            chkASoftware.Checked = false;
+            chkEGarantia.Checked = false;
+
+
+            txtNombre.Text = "";
+            txtTotal.Text = "";
+
+            cmbTipoDispositivo.Items.Clear();
+            cmbTipoDispositivo.Items.Add("Smartphone");
+            cmbTipoDispositivo.Items.Add("Tablet");
+            cmbTipoDispositivo.Items.Add("Consola");
+            cmbTipoDispositivo.SelectedIndex = 0;
+
+           
+
+
+        }
+
+        private void cmbTipoDispositivo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if(cmbTipoDispositivo.SelectedIndex == 0)
+            {
+                cmbPlazo.Items.Clear();
+
+                int d;
+                
+                for( d= 2; d < 11; d++)
+                {
+                    cmbPlazo.Items.Add(d);
+                    
+                }
+
+                cmbPlazo.SelectedIndex = 0;
+
+            }else {
+
+                 if(cmbTipoDispositivo.SelectedIndex == 1) 
+                {
+                    cmbPlazo.Items.Clear();
+
+                    int d; 
+
+                    for(d = 1;  d < 8; d++)
+                    {
+                        cmbPlazo.Items.Add(d);
+                    }
+                    cmbPlazo.SelectedIndex = 0;
+                }else{
+
+                    if(cmbTipoDispositivo.SelectedIndex == 2)
+                    {
+                        cmbPlazo.Items.Clear();
+                        
+                        int d; 
+                        
+                        for(d =5; d < 13; d++)
+                        {
+                            cmbPlazo.Items.Add(d);
+                        }
+                        cmbPlazo.SelectedIndex = 0;
+                    }
+
+                }
+
+
+            }
+
+
+        }
+
+        private void cmbPlazo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            if(txtNombre.Text != "")
+            {
+                btnCalcular.Enabled = true;
+            }
+            else
+            {
+                btnCalcular.Enabled = false;
+            }
+        }
+
+        private void btnReporte_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
